9f07c109b85caf0201aa92a5b4f98b3d3c26da26
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-12 16:09:50 +0100

    For basic_benchmark, name the configs instead of the Ruby version so we can collect data on the same Ruby in two configs at once; finer-grained skipping of Git repos; pass MIN_BENCH_X settings as explicit params instead of implicitly

--- basic_benchmark.rb
+++ basic_benchmark.rb
@@ -16,9 +16,64 @@
 require "optparse"
 require_relative "lib/yjit-metrics"
 
+# I prefer human-readable names for configs, where I can get them.
+# TODO: move Ruby config options here?
+TEST_RUBY_CONFIGS = {
+	debug_ruby_no_yjit: {
+		ruby: "ruby-yjit-metrics-debug",
+		opts: [ "--disable-yjit" ],
+	},
+	yjit_stats: {
+		ruby: "ruby-yjit-metrics-debug",
+		opts: [ "--yjit", "--yjit-stats" ],
+	},
+	prod_ruby_no_jit: {
+		ruby: "ruby-yjit-metrics-prod",
+		opts: [ "--disable-yjit" ],
+	},
+	prod_ruby_with_yjit: {
+		ruby: "ruby-yjit-metrics-prod",
+		opts: [ "--yjit" ],
+	},
+	prod_ruby_with_mjit: {
+		ruby: "ruby-yjit-metrics-prod",
+		opts: [ "--jit --disable-yjit" ],
+	},
+	ruby_27: {
+		ruby: "2.7.2",
+		opts: [],
+	},
+	ruby_27_with_mjit: {
+		ruby: "2.7.2",
+		opts: [ "--jit" ],
+	},
+}
+TEST_CONFIG_NAMES = TEST_RUBY_CONFIGS.keys.map(&:to_s)
+
+# Default settings for benchmark sampling
+DEFAULT_WARMUP_ITRS = 15       # Number of un-reported warmup iterations to run before "counting" benchmark runs
+DEFAULT_MIN_BENCH_ITRS = 10    # Minimum number of iterations to run each benchmark, regardless of time
+DEFAULT_MIN_BENCH_TIME = 10.0  # Minimum time in seconds to run each benchmark, regardless of number of iterations
+
+# Configuration for YJIT Rubies, debug and prod
+BASE_CONFIG_OPTIONS = [ "--disable-install-doc", "--disable-install-rdoc" ]
+YJIT_GIT_URL = "https://github.com/Shopify/yjit"
+YJIT_GIT_BRANCH = "main"
+PROD_YJIT_DIR = File.expand_path("#{__dir__}/../prod-yjit")
+DEBUG_YJIT_DIR = File.expand_path("#{__dir__}/../debug-yjit")
+
+# Configuration for yjit-bench
+YJIT_BENCH_GIT_URL = "https://github.com/Shopify/yjit-bench"
+YJIT_BENCH_GIT_BRANCH = "main" #"bench_setup_fixes"
+YJIT_BENCH_DIR = File.expand_path("#{__dir__}/../yjit-bench")
+
 # Defaults
 skip_git_updates = false
-warmup_itrs = 15
+warmup_itrs = DEFAULT_WARMUP_ITRS
+min_bench_itrs = DEFAULT_MIN_BENCH_ITRS
+min_bench_time = DEFAULT_MIN_BENCH_TIME
+DEFAULT_TEST_CONFIGS = [ :yjit_stats, :prod_ruby_with_yjit, :prod_ruby_no_jit ]
+configs_to_test = DEFAULT_TEST_CONFIGS
 
 OptionParser.new do |opts|
 	opts.banner = "Usage: basic_benchmark.rb [options] [<benchmark names>]"
@@ -30,6 +85,21 @@ OptionParser.new do |opts|
 	opts.on("--warmup-itrs=n", "Number of warmup iterations that do not have recorded per-run timings") do |n|
 		warmup_itrs = n
 	end
+
+	opts.on("--min-bench-time=t", "Number of seconds minimum to run real benchmark iterations, default: 10.0") do |t|
+		min_bench_time = t.to_f
+	end
+
+	opts.on("--min-bench-itrs=n", "Number of iterations minimum to run real benchmark iterations, default: 10") do |n|
+		min_bench_itrs = n.to_i
+	end
+
+	config_desc = "Comma-separated list of configurations to test" + "\n\t\t\tfrom: #{TEST_CONFIG_NAMES.join(", ")}\n\t\t\tdefault: #{DEFAULT_TEST_CONFIGS.join(",")}"
+	opts.on("--configs=CONFIGS", config_desc) do |configs|
+		configs_to_test = configs.split(",").map(&:strip).map(&:to_sym).uniq
+		bad_configs = configs_to_test - TEST_CONFIG_NAMES
+		raise "Requested test configuration(s) don't exist: #{bad_configs.inspect}!" unless bad_configs.empty?
+	end
 end.parse!
 
 benchmark_list = ARGV
@@ -38,7 +108,7 @@ extra_config_options = []
 if ENV["RUBY_CONFIG_OPTS"]
 	extra_config_options = ENV["RUBY_CONFIG_OPTS"].split(" ")
 elsif RUBY_PLATFORM["darwin"] && !`which brew`.empty?
-	# On Mac with homebrew, default to Homebrew's OpenSSL location if not otherwise specified
+	# On Mac with Homebrew, default to Homebrew's OpenSSL location if not otherwise specified
 	extra_config_options = [ "--with-openssl-dir=/usr/local/opt/openssl" ]
 end
 
@@ -58,17 +128,16 @@ unless installed_rubies.any? { |ruby_name| ruby_name.end_with?("/ruby-2.7.2") }
 end
 
 ### First, ensure up-to-date YJIT repos in debug configuration and prod configuration
-BASE_CONFIG_OPTIONS = [ "--disable-install-doc", "--disable-install-rdoc" ]
-YJIT_GIT_URL = "https://github.com/Shopify/yjit"
-YJIT_GIT_BRANCH = "main"
-PROD_YJIT_DIR = File.expand_path("#{__dir__}/../prod-yjit")
-DEBUG_YJIT_DIR = File.expand_path("#{__dir__}/../debug-yjit")
-unless skip_git_updates
+
+if !skip_git_updates && configs_to_test.any? { |config| TEST_RUBY_CONFIGS[config][:ruby] == "ruby-yjit-metrics-prod" }
 	YJITMetrics.clone_ruby_repo_with path: PROD_YJIT_DIR,
 	    git_url: YJIT_GIT_URL,
 	    git_branch: YJIT_GIT_BRANCH,
 	    install_to: CHRUBY_RUBIES + "/ruby-yjit-metrics-prod",
 	    config_opts: BASE_CONFIG_OPTIONS + extra_config_options
+end
+
+if !skip_git_updates && configs_to_test.any? { |config| TEST_RUBY_CONFIGS[config][:ruby] == "ruby-yjit-metrics-debug" }
 	YJITMetrics.clone_ruby_repo_with path: DEBUG_YJIT_DIR,
 		git_url: YJIT_GIT_URL,
 		git_branch: YJIT_GIT_BRANCH,
@@ -78,27 +147,29 @@ unless skip_git_updates
 end
 
 ### Second, ensure an up-to-date local yjit-bench checkout
-YJIT_BENCH_GIT_URL = "https://github.com/Shopify/yjit-bench"
-YJIT_BENCH_GIT_BRANCH = "main"
-YJIT_BENCH_DIR = File.expand_path("#{__dir__}/../yjit-bench")
-unless skip_git_updates
+
+if !skip_git_updates
 	YJITMetrics.clone_repo_with path: YJIT_BENCH_DIR, git_url: YJIT_BENCH_GIT_URL, git_branch: YJIT_BENCH_GIT_BRANCH
 end
 
 # For CI-style metrics collection we'll want timestamped results over time, not just the most recent.
 timestamp = Time.now.getgm.strftime('%F-%H%M%S')
 
-[ "ruby-yjit-metrics-debug", "ruby-yjit-metrics-prod", "2.7.2" ].each do |ruby|
+configs_to_test.each do |config|
+	ruby = TEST_RUBY_CONFIGS[config][:ruby]
+	ruby_opts = TEST_RUBY_CONFIGS[config][:opts]
 	yjit_results = YJITMetrics.run_benchmarks(
 		YJIT_BENCH_DIR,
 		TEMP_DATA_PATH,
-		ruby_opts: [],
+		with_chruby: ruby,
+		ruby_opts: ruby_opts,
 		benchmark_list: benchmark_list,
 		warmup_itrs: warmup_itrs,
-		with_chruby: ruby
+		min_benchmark_itrs: min_bench_itrs,
+		min_benchmark_time: min_bench_time
 		)
 
-	json_path = OUTPUT_DATA_PATH + "/basic_benchmark_#{ruby}_#{timestamp}.json"
+	json_path = OUTPUT_DATA_PATH + "/basic_benchmark_#{config}_#{timestamp}.json"
 	puts "Writing to JSON output file #{json_path}."
 	File.open(json_path, "w") { |f| f.write JSON.pretty_generate(yjit_results) }
 end
