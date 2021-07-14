5859fc8f49f6b47c41266f8a3ca8c3a330cbb5d0
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-14 11:14:46 +0100

    Initial test and Gemfile boilerplate

---
+++ test/metrics_test.rb
@@ -0,0 +1,10 @@
+require_relative "test_helper"
+
+class TestMeme < Minitest::Test
+    def setup
+    end
+
+    def test_lib
+        assert true
+    end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+require "minitest/autorun"
+
+require_relative "../lib/yjit-metrics"
+require_relative "../lib/yjit-metrics/bench-results"
