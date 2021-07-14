c4e5979aa5b5c6a688e861d4b03f79d1013d97ed
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-14 13:08:49 +0100

    WIP: More testing for reports

--- test/reporting_test.rb
+++ test/reporting_test.rb
@@ -1,6 +1,6 @@
 require_relative "test_helper"
 
-class TestMeme < Minitest::Test
+class TestBasicReporting < Minitest::Test
     def setup
     end
 
@@ -12,6 +12,16 @@ class TestMeme < Minitest::Test
 
         assert_equal [ "no_jit", "with_jit", "with_stats" ], results.available_configs.sort
         assert_equal [ "with_stats" ], results.configs_containing_yjit_stats
+    end
+
+    def test_creating_yjit_stats_multi_ruby_report
+        results = YJITMetrics::ResultSet.new
+        results.add_for_config "no_jit", JSON.load(File.read "test/data/basic_benchmark_prod_ruby_no_jit_2021-07-13-084249.json")
+        results.add_for_config "with_yjit", JSON.load(File.read "test/data/basic_benchmark_prod_ruby_with_yjit_2021-07-13-084249.json")
+        results.add_for_config "with_stats", JSON.load(File.read "test/data/basic_benchmark_yjit_stats_2021-07-13-084249.json")
+
+        report = YJITMetrics::YJITStatsMultiRubyReport.new [ "no_jit", "with_yjit", "yjit_stats" ], results
+        report.to_s
     end
 
     def test_calculation_with_synthetic_data
