62271a8464d008ed9e35357b33ec3311b4c8b04d
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-14 12:08:44 +0100

    Add a simple example reporting test

--- test/reporting_test.rb
+++ test/reporting_test.rb
@@ -13,4 +13,15 @@ class TestMeme < Minitest::Test
         assert_equal [ "no_jit", "with_jit", "with_stats" ], results.available_configs.sort
         assert_equal [ "with_stats" ], results.configs_containing_yjit_stats
     end
+
+    def test_calculation_with_synthetic_data
+        results = YJITMetrics::ResultSet.new
+        results.add_for_config "fake_ruby", JSON.load(File.read "test/data/synthetic_data.json")
+
+        report = YJITMetrics::YJITStatsExitReport.new ["fake_ruby"], results
+        report_text = report.to_s
+
+        assert report_text.include?("getinstancevariable exit reasons: \n    (all relevant counters are zero)"),
+            "Report should say that all getinstancevariable exit reasons are zero"
+    end
 end
