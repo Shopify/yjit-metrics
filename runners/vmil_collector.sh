72f142bbbb7cdaa0583521edd519e5b30d53ce78
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-14 16:07:15 +0100

    Remove nbody from vmil collector

--- runners/vmil_collector.sh
+++ runners/vmil_collector.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-./basic_benchmark.rb --skip-git-updates --warmup-itrs=15 --min-bench-time=60.0 --min-bench-itrs=50 --configs=yjit_stats,prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit railsbench optcarrot activerecord jekyll liquid-render psych-load lee nbody
+./basic_benchmark.rb --skip-git-updates --warmup-itrs=15 --min-bench-time=60.0 --min-bench-itrs=50 --configs=yjit_stats,prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit railsbench optcarrot activerecord jekyll liquid-render psych-load lee
