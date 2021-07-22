fddcac4276bfc833efa63f0d155495f46e121a31
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-22 12:56:27 +0100

    For runners, update to call setup.sh and for (commented out) git update section to be as small as reasonable

--- runners/vmil_warmup_collector.sh
+++ runners/vmil_warmup_collector.sh
@@ -7,9 +7,12 @@ RUNS=10
 BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=0 --runs=$RUNS --min-bench-time=0.1 --configs=prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit,truffleruby"
 YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=0 --min-bench-time=0.1 --configs=yjit_stats"
 
+# Running on Linux? Need to disable the appropriate things...
+sudo ./setup.sh
+
 # Do a Git update on all the Rubies?
 # Updating to head-of-master makes it hard to control config for real runs.
-#./basic_benchmark.rb --min-bench-time=0.1 --min-bench-itrs=1 --warmup-itrs=0 --configs=yjit_stats,prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit activerecord
+#./basic_benchmark.rb --min-bench-time=0.1 --min-bench-itrs=1 --warmup-itrs=0 --configs=yjit_stats,prod_ruby_with_yjit,truffleruby activerecord
 
 # yjit_stats run with same warmup and iteration count will get accurate counter and coverage values
 ./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=10  psych-load
