5836f48a7188218cf99b55badfb92cef17fbd5a0
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-29 17:03:09 +0100

    Final (?) changes to VMIL speedup runner-script for real runs

--- runners/vmil_speed_collector.sh
+++ runners/vmil_speed_collector.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-WARMUPS=15
+WARMUPS=500
 RUNS=10
 
 BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --runs=$RUNS --min-bench-time=0.1 --configs=prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit,truffleruby"
-YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.1 --configs=yjit_stats"
+#YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.1 --configs=yjit_stats"
 
 # Running on Linux? Need to disable the appropriate things...
 sudo ./setup.sh
@@ -16,23 +16,12 @@ sudo ./setup.sh
 #./basic_benchmark.rb --min-bench-time=0.1 --min-bench-itrs=1 --warmup-itrs=0 --configs=yjit_stats,prod_ruby_with_yjit,truffleruby activerecord
 
 # yjit_stats run with same warmup and iteration count will get accurate counter and coverage values
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=10  psych-load
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=6   30k_methods
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=15  30k_ifelse
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=17  lee
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=150 liquid-render
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=180 activerecord
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=5   optcarrot
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=3   jekyll
-./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS --min-bench-itrs=8   railsbench
-
-# These iteration counts are chosen to be in the general neighbourhood of 30 seconds on un-JITted CRuby
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=10  psych-load
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=6   30k_methods
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=15  30k_ifelse
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=17  lee
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=150 liquid-render
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=180 activerecord
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=5   optcarrot
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=3   jekyll
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=8   railsbench
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 psych-load
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_methods
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_ifelse
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 lee
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 liquid-render
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 activerecord
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 optcarrot
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 jekyll
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 railsbench
