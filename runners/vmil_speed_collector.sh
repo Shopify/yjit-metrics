c90349a8bcb046154299e977375e92570e5b4948
Noah Gibbs <noah.gibbs@shopify.com>
2021-07-29 23:35:29 +0100

    Reorder benchmarks into rough fastest-iter-to-slowest-iter order

--- runners/vmil_speed_collector.sh
+++ runners/vmil_speed_collector.sh
@@ -16,13 +16,12 @@ sudo ./setup.sh
 # Updating to head-of-master makes it hard to control config for real runs.
 #./basic_benchmark.rb --min-bench-time=0.1 --min-bench-itrs=1 --warmup-itrs=0 --configs=yjit_stats,prod_ruby_with_yjit,truffleruby activerecord
 
-# yjit_stats run with same warmup and iteration count will get accurate counter and coverage values
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 psych-load
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_methods
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_ifelse
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 lee
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 liquid-render
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 activerecord
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 liquid-render
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 lee
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_ifelse
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 psych-load
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 railsbench
+./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_methods
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 optcarrot
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 jekyll
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 railsbench
