b222e83cf25d4ec5e35531dcc881b0ee80949aac
Noah Gibbs <noah.gibbs@shopify.com>
2021-08-03 15:26:37 +0100

    Remove Jekyll benchmark from VMIL collector, reduce warmups, use MJIT in Ruby 3.0.2, single run

--- runners/vmil_speed_collector.sh
+++ runners/vmil_speed_collector.sh
@@ -2,19 +2,14 @@
 
 set -e
 
-WARMUPS=500
-RUNS=10
+WARMUPS=50
 
-BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --runs=$RUNS --min-bench-time=0.1 --on-error=report --configs=prod_ruby_with_yjit,prod_ruby_with_mjit,prod_ruby_no_jit"
+BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.01 --on-error=report --configs=prod_ruby_with_yjit,ruby_30_with_mjit,prod_ruby_no_jit"
 #YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.1 --configs=yjit_stats"
 
 # Running on Linux? Need to disable the appropriate things...
 # Since we're only doing 10 runs, I won't turn off ASLR.
 sudo ./setup.sh
-
-# Do a Git update on all the Rubies?
-# Updating to head-of-master makes it hard to control config for real runs.
-#./basic_benchmark.rb --min-bench-time=0.1 --min-bench-itrs=1 --warmup-itrs=0 --configs=yjit_stats,prod_ruby_with_yjit,truffleruby activerecord
 
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 activerecord
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 liquid-render
@@ -24,4 +19,3 @@ sudo ./setup.sh
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 railsbench
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_methods
 ./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 optcarrot
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 jekyll
