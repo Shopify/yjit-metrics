103cff4f6d3902c6d3a181bc7a018e333d177a43
Noah Gibbs <noah.gibbs@shopify.com>
2021-08-04 14:02:47 +0100

    More VMIL runner updates

--- runners/vmil_speed_collector.sh
+++ runners/vmil_speed_collector.sh
@@ -3,19 +3,22 @@
 set -e
 
 WARMUPS=50
+ITERATIONS=200
 
-BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.01 --on-error=report --configs=prod_ruby_with_yjit,ruby_30_with_mjit,prod_ruby_no_jit"
-#YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.1 --configs=yjit_stats"
+BASE_BENCH_OPTS="--skip-git-updates --warmup-itrs=$WARMUPS --min-bench-time=0.01 --on-error=report --configs=prod_ruby_with_yjit,ruby_30_with_mjit,prod_ruby_no_jit,truffleruby --min-bench-itrs=$ITERATIONS"
+YJIT_STATS_BENCH_OPTS="--skip-git-updates --warmup-itrs=0 --min-bench-time=0.1 --configs=yjit_stats --min-bench-itrs=1"
 
 # Running on Linux? Need to disable the appropriate things...
-# Since we're only doing 10 runs, I won't turn off ASLR.
 sudo ./setup.sh
 
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 activerecord
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 liquid-render
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 lee
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_ifelse
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 psych-load
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 railsbench
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 30k_methods
-./basic_benchmark.rb $BASE_BENCH_OPTS --min-bench-itrs=200 optcarrot
+./basic_benchmark.rb $YJIT_STATS_BENCH_OPTS activerecord liquid-render lee 30k_ifelse psych-load railsbench 30k_methods optcarrot
+
+# These are roughly ordered fastest-first.
+./basic_benchmark.rb $BASE_BENCH_OPTS activerecord
+./basic_benchmark.rb $BASE_BENCH_OPTS liquid-render
+./basic_benchmark.rb $BASE_BENCH_OPTS lee
+./basic_benchmark.rb $BASE_BENCH_OPTS 30k_ifelse
+./basic_benchmark.rb $BASE_BENCH_OPTS psych-load
+./basic_benchmark.rb $BASE_BENCH_OPTS railsbench
+./basic_benchmark.rb $BASE_BENCH_OPTS 30k_methods
+./basic_benchmark.rb $BASE_BENCH_OPTS optcarrot
