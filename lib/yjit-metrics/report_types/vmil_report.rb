48ca049ce3714d9b7bca85797b7f116f01b02b32
Noah Gibbs <noah.gibbs@shopify.com>
2021-08-04 10:14:29 +0100

    Replace 'speedup' with 'opt' in VMIL report for narrower column titles

--- lib/yjit-metrics/report_types/vmil_report.rb
+++ lib/yjit-metrics/report_types/vmil_report.rb
@@ -45,7 +45,7 @@ class YJITMetrics::VMILSpeedReport < YJITMetrics::VMILReport
         @benchmark_names.sort_by! { |bench_name| @yjit_stats[bench_name][0]["compiled_iseq_count"] }
 
         # Report contents
-        @headings = [ "bench", "YJIT (ms)", "YJIT RSD (%)", "MJIT (ms)", "MJIT RSD (%)", "No JIT (ms)", "No JIT RSD (%)", "YJIT speedup (%)", "YJIT speedup RSD (%)", "MJIT speedup (%)", "MJIT speedup RSD (%)", "% in YJIT" ]
+        @headings = [ "bench", "YJIT (ms)", "YJIT RSD (%)", "MJIT (ms)", "MJIT RSD (%)", "No JIT (ms)", "No JIT RSD (%)", "YJIT opt (%)", "YJIT opt RSD (%)", "MJIT opt (%)", "MJIT opt RSD (%)", "% in YJIT" ]
         @col_formats = [ "%s" ] + [ "%.1f", "%.2f" ] * 3 + [ "%.2f", "%.2f", "%.2f", "%.2f", "%.2f" ]
 
         @report_data = @benchmark_names.map do |benchmark_name|
@@ -100,7 +100,8 @@ class YJITMetrics::VMILSpeedReport < YJITMetrics::VMILReport
 
     def to_s
         format_as_table(@headings, @col_formats, @report_data) +
-            "\nRSD is relative standard deviation (stddev / mean), expressed as a percent.\n"
+            "\nRSD is relative standard deviation (stddev / mean), expressed as a percent.\n" +
+            "Opt is optimization (speedup) expressed as % faster.\n"
     end
 
     def write_file(filename)
