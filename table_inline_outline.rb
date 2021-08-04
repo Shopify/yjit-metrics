18f314dd206919512f8d74d58240e4d853e4756f
Kevin Newton <kddnewton@gmail.com>
2021-08-04 11:19:26 -0400

    Generate tables for VMIL

---
+++ table_inline_outline.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Generates a table of the inline and outlined code size per benchmark.
+#
+# To execute, run ./table_inline_outline.rb data/path/to/yjit_stats.json
+#
+
+require "erb"
+require "json"
+
+size = -> (value) {
+  units = ["B", "kB", "MB", "GB"]
+  unit = units.shift
+
+  while value > 1024
+    value /= 1024.0
+    unit = units.shift
+  end
+
+  "#{value.round(2)} #{unit}"
+}
+
+data =
+  JSON.parse(File.read(ARGV.first))["yjit_stats"].map do |name, (values, *)|
+    [name.gsub("_", "\\_"), size[values["inline_code_size"]], size[values["outlined_code_size"]]]
+  end
+
+data.sort_by!(&:first)
+data.map! { |row| "#{row.join(" & ")} \\\\\n\\hline" }
+
+puts ERB.new(DATA.read).result_with_hash(data: data)
+
+__END__
+\begin{table*}
+\begin{center}
+\begin{tabular}{||c c c||}
+\hline
+Benchmark & Inline code size & Outlined code size \\ [0.5ex]
+\hline\hline
+<%= data.join("\n") %>
+\end{tabular}
+\end{center}
+\caption{Inline and outlined code size}
+\end{table*}
---
+++ table_iseqs_compiled.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Generates a table of the number of compiled iseqs per benchmark. Also includes
+# a space for description, but doesn't include it here (it'll be written into
+# the paper so it can be edited without going through a PR).
+#
+# To execute, run ./table_iseqs_compiled.rb data/path/to/yjit_stats.json
+#
+
+require "erb"
+require "json"
+
+data =
+  JSON.parse(File.read(ARGV.first))["yjit_stats"].map do |name, (values, *)|
+    [name.gsub("_", "\\_"), "", values["compiled_iseq_count"]]
+  end
+
+data.sort_by!(&:first)
+data.map! { |row| "#{row.join(" & ")} \\\\\n\\hline" }
+
+puts ERB.new(DATA.read).result_with_hash(data: data)
+
+__END__
+\begin{table*}
+\begin{center}
+\begin{tabular}{||c c c||}
+\hline
+Benchmark & Description & Compiled iseqs \\ [0.5ex]
+\hline\hline
+<%= data.join("\n") %>
+\end{tabular}
+\end{center}
+\caption{Compiled iseqs}
+\end{table*}
