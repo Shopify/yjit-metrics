82e6fd77bd63e5ffe7b15d8aeaf33fdd4fbea53d
Noah Gibbs <noah.gibbs@shopify.com>
2021-08-25 13:55:51 +0100

    Early version of speed report, no labels yet

--- lib/yjit-metrics/report_types/comparative_reports.rb
+++ lib/yjit-metrics/report_types/comparative_reports.rb
@@ -8,18 +8,19 @@ class YJITMetrics::CompareReport < YJITMetrics::YJITStatsReport
         matching_configs[0]
     end
 
-    def look_up_data_by_ruby(in_runs: false, no_jit: true, truffle: false)
+    # Include Truffle data only if we can find it
+    def look_up_data_by_ruby(in_runs: false)
         @with_yjit_config = exactly_one_config_with_name(@config_names, "with_yjit", "with-YJIT")
         @with_mjit_config = exactly_one_config_with_name(@config_names, "with_mjit", "with-MJIT")
-        @no_jit_config    = exactly_one_config_with_name(@config_names, "no_jit", "no-JIT") if no_jit
-        @truffle_config   = exactly_one_config_with_name(@config_names, "truffleruby", "Truffle") if truffle
+        @no_jit_config    = exactly_one_config_with_name(@config_names, "no_jit", "no-JIT")
+        @truffle_config   = exactly_one_config_with_name(@config_names, "truffleruby", "Truffle")
 
         @configs_with_human_names = [
+            ["No JIT", @no_jit_config],
             ["YJIT", @with_yjit_config],
             ["MJIT", @with_mjit_config],
         ]
-        @configs_with_human_names.push(["Truffle", @truffle_config]) if truffle
-        @configs_with_human_names.push(["No JIT", @no_jit_config]) if no_jit
+        @configs_with_human_names.push(["Truffle", @truffle_config]) if @truffle_config
 
         # Grab relevant data from the ResultSet
         @times_by_config = {}
@@ -53,7 +54,7 @@ class YJITMetrics::CompareSpeedReport < YJITMetrics::CompareReport
         # Set up the YJIT stats parent class
         super
 
-        look_up_data_by_ruby(no_jit: true, truffle: true)
+        look_up_data_by_ruby
 
         no_stats_benchmarks = @benchmark_names.select { |bench_name| !@yjit_stats[bench_name] || !@yjit_stats[bench_name][0] || @yjit_stats[bench_name][0].empty? }
         unless no_stats_benchmarks.empty?
@@ -108,7 +109,7 @@ class YJITMetrics::CompareSpeedReport < YJITMetrics::CompareReport
 
                 this_config_mean = @mean_by_config[config][-1]
                 this_config_rel_stddev = @rsd_by_config[config][-1]
-                speed_ratio = no_jit_mean / this_config_mean
+                speed_ratio = this_config_mean / no_jit_mean
                 speed_rel_stddev = Math.sqrt(no_jit_rel_stddev * no_jit_rel_stddev + this_config_rel_stddev * this_config_rel_stddev)
                 @speedup_by_config[config].push [ speed_ratio, speed_rel_stddev ]
             end
@@ -140,19 +141,105 @@ class YJITMetrics::CompareSpeedReport < YJITMetrics::CompareReport
             "Spd is the speed (iters/second) of the optimised implementation -- 2.0x would be twice as many iters per second.\n"
     end
 
-    def write_file(filename)
+    def to_pct(ratio)
+        (ratio * 100.0).to_s + "%"
+    end
+
+    # These will be assigned in order to each Ruby
+    RUBY_BAR_COLOURS = [ "blue", "orange", "green", "red" ]
+
+    def svg_object
         # If we render a comparative report to file, we need victor for SVG output.
         require "victor"
 
-        svg = Victor::SVG.new template: :minimal
-        # ...
-        @svg_body = svg.render
+        svg = Victor::SVG.new template: :minimal, style: { }  # background: '#ddd'
+
+        axis_colour = "#000"
+        background_colour = "#EEE"
+
+        # Reserve the left 15% of the width for the axis scale numbers. Right 5% is whitespace.
+        left_axis_width = 0.15
+        right_whitespace = 0.05
+
+        # Reserve the top 5% as whitespace, and the bottom 20% for benchmark names.
+        bottom_key_height = 0.20
+        top_whitespace = 0.05
+
+        plot_left_edge = left_axis_width
+        plot_top_edge = top_whitespace
+        plot_width = 1.0 - left_axis_width - right_whitespace
+        plot_height = 1.0 - bottom_key_height - top_whitespace
+
+        svg.rect x: to_pct(plot_left_edge), y: to_pct(plot_top_edge),
+            width: to_pct(plot_width), height: to_pct(plot_height),
+            stroke: axis_colour, fill: background_colour
+
+
+        # Basic info on Ruby configs and benchmarks
+        ruby_configs = @configs_with_human_names.map { |name, config| config }
+        ruby_config_bar_colour = Hash[ruby_configs.zip(RUBY_BAR_COLOURS)]
+        n_configs = ruby_configs.size
+        n_benchmarks = @benchmark_names.size
+
+
+        # How high to speedup ratios go?
+        max_speedup_ratio = @speedup_by_config.values.map { |speedup_by_bench| speedup_by_bench.map(&:first).max }.max
+
+
+        # Now let's calculate some widths...
+
+        # Within each benchmark's horizontal span we'll want 3 or 4 bars plus a bit of whitespace.
+        # And we'll reserve 5% of the plot's width for whitespace on the far left and again on the far right.
+        plot_padding_ratio = 0.05
+        each_bench_width = (plot_width * (1.0 - 2 * plot_padding_ratio)) / n_benchmarks
+        bar_width = each_bench_width / (n_configs + 1)
+
+        first_bench_left_edge = plot_left_edge + (plot_width * plot_padding_ratio)
+        bench_left_edge = (0...n_benchmarks).map { |idx| first_bench_left_edge + idx * each_bench_width }
+
+
+        # And some heights...
+        plot_top_whitespace = 0.05 * plot_height
+        plot_effective_top = plot_top_edge + plot_top_whitespace
+        plot_effective_height = plot_height - plot_top_whitespace
+
+
+        # Okay. Now let's plot a lot of boxes and whiskers.
+        @benchmark_names.each.with_index do |bench_name, bench_idx|
+            no_jit_mean = @mean_by_config[@no_jit_config][bench_idx]
+
+            bars_width_start = bench_left_edge[bench_idx]
+            ruby_configs.each.with_index do |config, config_idx|
+                if config == @no_jit_config
+                    speedup = 1.0 # No-JIT is always exactly 1x No-JIT
+                    rsd = @rsd_by_config[@no_jit_config][bench_idx]
+                else
+                    speedup, rsd = @speedup_by_config[config][bench_idx]
+                end
+                bar_height_ratio = speedup / max_speedup_ratio;
+
+                svg.rect \
+                    x: to_pct(bars_width_start + config_idx * bar_width),
+                    y: to_pct(plot_effective_top + (1.0 - bar_height_ratio) * plot_effective_height),
+                    width: to_pct(bar_width),
+                    height: to_pct(bar_height_ratio * plot_effective_height),
+                    fill: ruby_config_bar_colour[config]
+            end
+        end
+
+        svg
+    end
+
+    def write_file(filename)
+        require "victor"
+
+        @svg = svg_object
 
         script_template = ERB.new File.read(__dir__ + "/../report_templates/compare_speed.html.erb")
         html_output = script_template.result(binding) # Evaluate an Erb template with template_settings
         File.open(filename + ".html", "w") { |f| f.write(html_output) }
 
-        write_to_csv(filename + ".csv", [@headings] + report_table_data)
+        #write_to_csv(filename + ".csv", [@headings] + report_table_data)
     end
 
 end
