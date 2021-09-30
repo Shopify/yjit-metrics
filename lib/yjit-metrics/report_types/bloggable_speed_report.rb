77145e98e5449f5560cd308764532fee1916a435
Noah Gibbs <noah.gibbs@shopify.com>
2021-09-30 12:52:28 +0100

    Properly use the calculated category index

--- lib/yjit-metrics/report_types/bloggable_speed_report.rb
+++ lib/yjit-metrics/report_types/bloggable_speed_report.rb
@@ -604,7 +604,7 @@ class YJITMetrics::SpeedHeadlineReport < YJITMetrics::BloggableSingleReport
             category_idx = 1 # Default to misc
             category_idx = 0 if BENCHMARK_METADATA[bench_name] && BENCHMARK_METADATA[bench_name][:category] == :headline
             category_idx = 2 if BENCHMARK_METADATA[bench_name] && BENCHMARK_METADATA[bench_name][:category] == :micro
-            [ BENCHMARK_METADATA[bench_name][:category] == :headline ? 0 : BENCHMARK_METADATA[bench_name][:category] == :micro ? 2 : 1,
+            [ category_idx,
               #-@yjit_stats[bench_name][0]["compiled_iseq_count"],
               bench_name ] }
 
