f0f4370962ef3d4b2d176ec9b03060381c20003e
Noah Gibbs <noah.gibbs@shopify.com>
2021-11-04 12:31:33 +0000

    Add a use_gemfile declaration to the harness DSL for the new benchmark changes

--- metrics-harness/harness.rb
+++ metrics-harness/harness.rb
@@ -49,6 +49,31 @@ def ruby_metadata
     }
 end
 
+# Specify a Gemfile and directory to use; install gems; do any extra per-benchmark setup.
+# This varies from the yjit-bench harness method because it specifies one exact Bundler version.
+def use_gemfile(extra_bundled_setup: nil)
+  chruby_stanza = ""
+  if ENV['RUBY_ROOT']
+    ruby_name = ENV['RUBY_ROOT'].split("/")[-1]
+    chruby_stanza = "chruby && chruby #{ruby_name} && "
+  end
+
+  # Source Shopify-located chruby if it exists to make sure this works in Shopify Mac dev tools.
+  # Use bash -l to propagate non-Shopify-style chruby config.
+  cmd = "/bin/bash -l -c '[ -f /opt/dev/dev.sh ] && . /opt/dev/dev.sh; #{chruby_stanza}bundle install'"
+  if extra_bundled_setup
+    cmd += " && #{extra_bundled_setup}"
+  end
+  puts "Command: #{cmd}"
+  success = system(cmd)
+  unless success
+    raise "Couldn't set up benchmark in #{in_dir.inspect}!"
+  end
+
+  # Need to be in the appropriate directory
+  require "bundler/setup"
+end
+
 # Takes a block as input
 def run_benchmark(num_itrs_hint)
   times = []
