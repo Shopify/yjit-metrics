80aa08a7ff05f8f320fe4943892a16d5fe54a220
Noah Gibbs <noah.gibbs@shopify.com>
2021-11-24 13:44:13 +0000

    In timeline report, sort by visibility *and name* for order stability in the HTML source. Doesn't affect final appearance.

--- lib/yjit-metrics/timeline_report_types/blog_timeline.rb
+++ lib/yjit-metrics/timeline_report_types/blog_timeline.rb
@@ -28,7 +28,10 @@ class BlogTimelineReport < YJITMetrics::TimelineReport
 
             @series.push({ config: config, benchmark: benchmark, name: "#{config}-#{benchmark}", visible: visible, data: all_points.compact })
         end
-        @series.sort_by! { |s| s[:visible] ? 0 : 1 }
+        @series.sort_by! do |s|
+            visi = s[:visible] ? 0 : 1
+            [ visi, s[:name] ]
+        end
     end
 
     def write_file(file_path)
