24fe7ee4219027b40f9c24a01de7b9b9fcfc94bf
Noah Gibbs (and/or Benchmark CI) <noah.gibbs@shopify.com>
2022-10-10 10:54:26 +0000

    Move commit-and-push benchmark data logic into a shellscript, not GHActions. Clean Gemfile.lock and reports before pull/commit/push.

---
+++ continuous_reporting/gh_tasks/commit_benchmark_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+set -e
+
+# We'll use a released Ruby here to maximize the odds that the test harness runs even when YJIT is broken.
+# Also we're gonna be messing with the installed prerelease Rubies a fair bit.
+chruby 3.0.2
+
+cd ~/ym/yjit-metrics-pages
+
+# We've probably run reporting tests here. That leaves certain files dirty, but we don't want to check them in.
+# We do *not* want to "git clean" because we have uncommitted test data.
+git checkout Gemfile.lock _includes/reports
+
+git pull
+git add raw_benchmark_data
+git commit -m "`uname -p` benchmark results"
+git push
+
+echo "Committed and pushed benchmark data successfully."
