77c2073677d44b82932dc374b1992179f93294ef
Noah Gibbs <noah.gibbs@shopify.com>
2023-02-01 11:20:03 +0000

    Do full rebuild for stats-yjit, not just prod and debug

--- continuous_reporting/gh_tasks/fully_rebuild_rubies.sh
+++ continuous_reporting/gh_tasks/fully_rebuild_rubies.sh
@@ -10,11 +10,13 @@ chruby 3.0.2
 # This will uninstall all gems, among other side effects.
 rm -rf ~/.rubies/ruby-yjit-metrics-debug/*
 rm -rf ~/.rubies/ruby-yjit-metrics-prod/*
+rm -rf ~/.rubies/ruby-yjit-metrics-stats/*
 rm -rf ~/.gem/ruby/3.2.0
 
 # This isn't a 100% cleanup -- no ./configure, for instance, which basic_benchmark.rb will only do if the current config looks wrong
 cd ~/ym/prod-yjit && git clean -d -x -f
 cd ~/ym/debug-yjit && git clean -d -x -f
+cd ~/ym/stats-yjit && git clean -d -x -f
 
 cd ~ubuntu/ym/yjit-bench
 find . -wholename "*tmp/cache/bootsnap" -print0 | xargs -0 rm -r || echo OK
