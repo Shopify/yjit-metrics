57fa4b826c3e9405a891029fce5d9c95cb597387
Noah Gibbs <noah.gibbs@shopify.com>
2023-02-21 12:06:47 +0000

    Slightly improved benchmark data upload, but also do benchmark data upload right after benchmarking

--- continuous_reporting/gh_tasks/commit_benchmark_data.sh
+++ continuous_reporting/gh_tasks/commit_benchmark_data.sh
@@ -6,6 +6,7 @@ set -e
 # Also we're gonna be messing with the installed prerelease Rubies a fair bit.
 chruby 3.0.2
 
+# If there is uncommitted data after a benchmark run, get it into raw_benchmarks where it belongs
 cd ~/ym/yjit-metrics
 ruby continuous_reporting/file_benchmark_data_into_raw.rb -d continuous_reporting/data
 
@@ -14,13 +15,16 @@ cd ~/ym/yjit-metrics-pages
 # Anything that's been added to a pending commit should be un-added
 git restore --staged .
 
-# We've probably run reporting tests here. That leaves certain files dirty, but we don't want to check them in.
+# We've probably run reporting tests here. Get rid of uncommitted changes to reports, etc.
 git checkout Gemfile.lock _includes/reports reports
 
-# Clean only the report dirs because we might have uncommitted test data.
+# Clean only the report dirs. We might have uncommitted test data, so no cleaning data dirs.
 git clean -d -f _includes/reports reports
 
-git pull
+# Sometimes we make a commit at the exact same time as the x86 worker and then we can't
+# push it. If that has happened, rebase our data commit back over top of the new head commit.
+git pull --rebase
+
 # This should commit only if there's anything to commit, but not fail if empty
 git add raw_benchmark_data
 git commit -m "`uname -p` benchmark results" || echo "Commit is empty?"
