bb4f47c071ea6630761e1e458fefcd0fe01f6757
Noah Gibbs <noah.gibbs@shopify.com>
2023-03-21 12:45:29 +0000

    More WIP on new static site generator

--- site/framework/render.rb
+++ site/framework/render.rb
@@ -11,9 +11,9 @@ SPECIAL_DIRS = [ "_layouts", "_includes", "_sass" ]
 TOPLEVEL_SKIPPED = [ "_config.yml" ]
 
 # TODO: handle _sass dir - just pregenerate up-front?
-# TODO: render collections
 
 def redcarpet_render_markdown(text)
+  require "redcarpet"
   @md_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
   @md_renderer.render(text)
 end
@@ -76,11 +76,34 @@ def render_file(path, metadata)
   #text = erb_tmpl.result(dsl.send(:binding))
 
   if front_matter_data[:layout]
-    STDERR.puts "Rendering with layout #{front_matter_data[:layout].inspect} and content #{text[0..10].inspect}"
     return render_file("_layouts/#{front_matter_data[:layout]}.erb", metadata.merge(content: text))
   end
 
   text
+end
+
+def render_file_to_location(in_path, out_dir, metadata)
+  ext = File.extname(in_path)
+  filename = File.split(in_path)[-1]
+
+  case ext
+  when ".erb"
+    new_contents = render_file(in_path, metadata)
+    File.write("#{out_dir}/#{filename.delete_suffix(".erb")}", new_contents)
+  else
+    FileUtils.mkdir_p(out_dir)
+    FileUtils.cp in_path, "#{out_dir}/#{filename}"
+  end
+end
+
+def render_collection_item_to_location(item, out_dir, metadata)
+  layout = item.layout
+  unless layout
+    raise "Can't find layout for item #{item["name"].inspect} / #{item.inspect}!"
+  end
+
+  contents = render_file("_layouts/#{layout}.erb", metadata.merge(:page => item))
+  File.write("#{out_dir}/#{item[:name]}", contents)
 end
 
 def parse_collections
@@ -89,6 +112,7 @@ def parse_collections
     out[coll] = []
     Dir["_#{coll}/*.md"].each do |file_w_frontmatter|
       item_data, _line, _content = read_file(file_w_frontmatter)
+      item_data[:name] = file_w_frontmatter.split("/")[-1].gsub("_", "-") # Ah, Jekyll. There's probably some deep annoying meaning to why this is needed.
       out[coll] << OpenStruct.new(item_data)
     end
   end
@@ -103,7 +127,8 @@ def build_site
   FileUtils.rm_rf "_site"
   Dir.mkdir "_site"
 
-  site_var = OpenStruct.new parse_collections
+  coll_data = parse_collections
+  site_var = OpenStruct.new coll_data
   metadata = {}
   metadata[:site] = site_var
 
@@ -118,18 +143,17 @@ def build_site
       raise "Unexpected repo path starting with underscore: #{repo_file.inspect}"
     end
 
-    # Note: detect and render markdown
-    # Note: File.extname/File.basename
+    out_dir = "_site/#{File.dirname repo_file}"
+    render_file_to_location(repo_file, out_dir, metadata)
+  end
 
-    if repo_file.end_with?(".erb")
-      STDERR.puts "ERB generate: #{repo_file.inspect}"
-      out_file = File.join("_site", repo_file).delete_suffix(".erb")
-      new_contents = render_file(repo_file, metadata)
-      File.write(out_file, new_contents)
-    else
-      out_loc = "_site/#{repo_file}"
-      FileUtils.mkdir_p(File.dirname(out_loc))
-      FileUtils.cp(repo_file, out_loc)
+  # Now build entries for the collections
+  COLLECTIONS.each do |collection|
+    # Output URL is /:collection_name:/:item_name:
+    out_dir = "_site/#{collection}"
+    Dir.mkdir out_dir
+    coll_data[collection].each do |item|
+      render_collection_item_to_location(item, out_dir, metadata)
     end
   end
 end
