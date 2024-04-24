5258f38e80a5b5974b6f6f95d93684b5d9920143
Randy Stauner <randy.stauner@shopify.com>
2024-04-24 11:48:48 -0700

    Handle BENCH_TYPE correctly in shell script

--- continuous_reporting/gh_tasks/generate_bench_params.sh
+++ continuous_reporting/gh_tasks/generate_bench_params.sh
@@ -11,6 +11,6 @@ cd ~/ym/yjit-metrics
 git checkout main && git pull
 
 # No timestamp given, default to right now
-ruby continuous_reporting/create_json_params_file.rb --full-rebuild=$FULL_REBUILD --bench-type=$BENCH_TYPE --yjit-metrics-name=$YJIT_METRICS_NAME --yjit-metrics-repo=$YJIT_METRICS_REPO --yjit-bench-name=$YJIT_BENCH_NAME --yjit-bench-repo=$YJIT_BENCH_REPO --cruby-name=$CRUBY_NAME --cruby-repo=$CRUBY_REPO --benchmark-data-dir=$BENCH_DATA_DIR
+ruby continuous_reporting/create_json_params_file.rb --full-rebuild=$FULL_REBUILD --bench-type="$BENCH_TYPE" --yjit-metrics-name=$YJIT_METRICS_NAME --yjit-metrics-repo=$YJIT_METRICS_REPO --yjit-bench-name=$YJIT_BENCH_NAME --yjit-bench-repo=$YJIT_BENCH_REPO --cruby-name=$CRUBY_NAME --cruby-repo=$CRUBY_REPO --benchmark-data-dir=$BENCH_DATA_DIR
 
 echo "Generated params successfully."
