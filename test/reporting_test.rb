8c4fb09bb9343a7ed77e10c7aaaa5cc482482c1d
Randy Stauner <randy.stauner@shopify.com>
2024-05-24 12:13:40 -0700

    Remove commented test for report that was removed long ago
    Class being smoke tested was removed in fdf330a9e3f4abeb6bff866b7a5ab8b437c1f1e9

--- test/reporting_test.rb
+++ test/reporting_test.rb
@@ -1,9 +1,6 @@
 require_relative "test_helper"
 
 class TestBasicReporting < Minitest::Test
-    def setup
-    end
-
     def test_load_multiple_real_data
         results = YJITMetrics::ResultSet.new
         results.add_for_config "no_jit", JSON.load(File.read "test/data/2021-09-13-100043_basic_benchmark_prod_ruby_no_jit.json")
@@ -41,16 +38,4 @@ class TestBasicReporting < Minitest::Test
         report = YJITMetrics::YJITStatsExitReport.new [ "yjit_stats" ], results
         report.to_s
     end
-
-    # VMIL report is basically historical at this point, and should probably be archived or removed
-#    def test_creating_vmil_report
-#        results = YJITMetrics::ResultSet.new
-#        results.add_for_config "no_jit", JSON.load(File.read "test/data/2021-09-13-100043_basic_benchmark_prod_ruby_no_jit.json")
-#        results.add_for_config "with_yjit", JSON.load(File.read "test/data/2021-09-13-100043_basic_benchmark_prod_ruby_with_yjit.json")
-#        results.add_for_config "with_mjit", JSON.load(File.read "test/data/2021-09-13-100043_basic_benchmark_prod_ruby_with_mjit.json")
-#        results.add_for_config "with_stats", JSON.load(File.read "test/data/2021-09-13-100043_basic_benchmark_yjit_stats.json")
-#
-#        report = YJITMetrics::VMILReport.new [ "no_jit", "with_yjit", "with_mjit", "with_stats" ], results
-#        report.to_s
-#    end
 end
