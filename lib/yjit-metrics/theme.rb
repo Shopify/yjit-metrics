563eb4931b6d8c3935206fc45b281593d2a899c6
Randy Stauner <randy.stauner@shopify.com>
2024-05-29 16:48:24 -0700

    Enable aliases in theme yaml for color reuse

--- lib/yjit-metrics/theme.rb
+++ lib/yjit-metrics/theme.rb
@@ -5,7 +5,8 @@ require "yaml"
 # Style information stored in root theme.yaml file.
 module YJITMetrics
   module Theme
-    CONFIG = YAML.safe_load_file(File.expand_path("../../theme.yaml", __dir__), symbolize_names: true)
+    FILE = File.expand_path("../../theme.yaml", __dir__)
+    CONFIG = YAML.safe_load_file(FILE, aliases: true, symbolize_names: true)
 
     # Define methods for each key in the yaml hash.
     module_eval CONFIG.map { |key, _val| "def self.#{key}; CONFIG.fetch(:#{key}); end" }.join("\n")
