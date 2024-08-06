ddf4182960d9de2a6d3ff71ef731c7947f75262c
Randy Stauner <randy.stauner@shopify.com>
2024-08-06 12:42:10 -0700

    Disable hwp and set scaling governor in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,18 @@ set -e
 
 setup-cpu () {
   # Keep commands simple so that they can be copied and pasted from this file with ease.
+  # TODO: Do we want to limit C-states in grub and rebuild the grub config?
 
   echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
   echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct
+
+  # hwp_dynamic_boost may not exist if disabled at boot time
+  # (with `intel_pstate=no_hwp` in the kernel command line).
+  if [[ -r /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost ]]; then
+    echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost
+  fi
+
+  echo performance | sudo tee /sys/devices/system/cpu/cpu"$((`nproc` - 1))"/cpufreq/energy_performance_preference /sys/devices/system/cpu/cpu"$((`nproc` - 1))"/cpufreq/scaling_governor
 }
 
 setup-packages () {
