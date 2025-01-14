449610bf95f987a7554de8203e7318353885f3d4
Randy Stauner <randy.stauner@shopify.com>
2025-01-14 08:24:53 -0700

    Remove usage of removed arg
    Missed this one during the cleanup

--- continuous_reporting/gh_tasks/run_benchmarks.sh
+++ continuous_reporting/gh_tasks/run_benchmarks.sh
@@ -6,6 +6,6 @@ cd ~/ym/yjit-metrics
 
 bundle
 
-ruby continuous_reporting/benchmark_and_update.rb --no-gh-issue --bench-params=$BENCH_PARAMS
+ruby continuous_reporting/benchmark_and_update.rb --bench-params=$BENCH_PARAMS
 
 echo "Completed benchmarking successfully."
