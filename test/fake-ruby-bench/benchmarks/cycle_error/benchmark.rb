9f8b6c6eb0f59152d7560c7301305ed6c3714082
Randy Stauner <randy.stauner@shopify.com>
2025-10-15 08:47:24 -0700

    Fix path to ruby-bench in tests

--- test/fake-ruby-bench/benchmarks/cycle_error/benchmark.rb
+++ test/fake-ruby-bench/benchmarks/cycle_error/benchmark.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Use real ruby-bench harness loader.
-real_yjit_bench = File.expand_path('../../../../../ruby-bench', __dir__)
+real_yjit_bench = File.expand_path('../../../../build/ruby-bench', __dir__)
 require "#{real_yjit_bench}/harness/loader.rb"
 
 Dir.chdir __dir__
