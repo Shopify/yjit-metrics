ec28348319c3a84b3e28679c9e1d9c179d91396a
Matt Valentine-House <matt@eightbitraptor.com>
2025-12-09 15:26:40 +0000

    Add a script that will clone rubybench and run the benchmarks

--- continuous_reporting/on_demand/dispatch.sh
+++ continuous_reporting/on_demand/dispatch.sh
@@ -22,6 +22,9 @@ do:benchmark () {
   # Some of the cpu settings do not persist across reboots and need to be set again.
   "$cr_dir"/../setup.sh cpu
 
+  # Clone and run the scripts using the rubybench/rubybench runner.
+  "$cr_dir"/gh_tasks/run_rubybench.sh
+
   # This script needs BENCH_PARAMS env var.
   # It may exit non-zero but still produce some benchmarks
   # so preserve the exit status but proceed as if successful.
