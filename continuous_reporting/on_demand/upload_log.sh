66ffdcbd7d5db3fb7db000f32d3914f12f3e338b
Randy Stauner <randy.stauner@shopify.com>
2026-01-22 16:59:18 -0700

    Set content-type for logs to text/plain so it will show in browser

--- continuous_reporting/on_demand/upload_log.sh
+++ continuous_reporting/on_demand/upload_log.sh
@@ -9,7 +9,7 @@ basename="${logfile##*/}"
 s3_key="${INSTANCE_NAME:-unknown}/$basename"
 s3_uri="s3://${S3_LOGS_BUCKET}/$s3_key"
 
-aws s3 cp "$logfile" "$s3_uri" >&2
+aws s3 cp "$logfile" "$s3_uri" --content-type text/plain >&2
 
 url=$(aws s3 presign "$s3_uri" --expires-in 604800)
 
